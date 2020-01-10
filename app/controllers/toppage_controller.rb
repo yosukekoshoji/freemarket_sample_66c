f59219a351aebcd78f39cc654666004c769b2571
yosukekoshoji <58423182+yosukekoshoji@users.noreply.github.com>
2020-01-10 12:18:28 +0900

    マイページ実装
    途中

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -2,4 +2,7 @@ class ToppageController < ApplicationController
 
   def index
   end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root "toppage#index"
+  root "toppage#show"
   
 end
