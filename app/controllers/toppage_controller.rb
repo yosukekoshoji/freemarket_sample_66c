356804510e46aa939fcc365d43ad48708f860199
hiroshi0918 <58413665+hiroshi0918@users.noreply.github.com>
2020-01-12 03:19:54 +0900

    完了

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -2,4 +2,8 @@ class ToppageController < ApplicationController
 
   def index
   end
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root "toppage#index"
+  root "toppage#show"
 end
