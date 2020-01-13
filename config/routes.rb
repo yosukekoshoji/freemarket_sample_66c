e703546282e8d91dd51592778deb4e92c37191af
hiroshi0918 <58413665+hiroshi0918@users.noreply.github.com>
2020-01-13 16:20:49 +0900

    ユーザープロフィール編集画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   root "toppage#index"
-  resources :mypages,only:[:index]
+  resources :mypages,only:[:index, :edit]
   resources :items,only:[:index,:show]
 end
