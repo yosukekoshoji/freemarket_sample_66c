90ceaeca0de40b616a2ef84e147ff39f0c606932
yosukekoshoji <58423182+yosukekoshoji@users.noreply.github.com>
2020-01-15 12:56:45 +0900

    本人確認ページ（DB削除済み）

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_083147) do
-
-  create_table "addresses", force: :cascade do |t|
-    t.integer "prefecture_id"
-    t.string "city"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
