2277a66d89655d115fbe28c36d66b065a0a659f9
hiroshi0918 <58413665+hiroshi0918@users.noreply.github.com>
2020-01-15 17:32:10 +0900

    コメントアウト外し

--- db/migrate/20200114041630_create_items.rb
+++ db/migrate/20200114041630_create_items.rb
@@ -1,8 +1,8 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      # t.references :seller, foreign_key: true
-      # t.references :buyer, foreign_key: true
+      t.references :seller, foreign_key: true
+      t.references :buyer, foreign_key: true
       t.string :name, null: false, index: true
       t.integer :price, null: false
       t.date :shipping_date, null: false
